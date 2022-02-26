fb2145738832a37161da914be8670e91e1ddb114
Michael Carter <mikeaaroncarter@gmail.com>
2022-02-26 12:42:03 -0800

    Reindent file spaces to tabs

--- assets/js/kahoycrafts.js
+++ assets/js/kahoycrafts.js
@@ -5,24 +5,24 @@
 
 		$('.owl-carousel').owlCarousel({
 			nav: true,
-		    loop: true,
-		    margin: 10,
-		    autoplay: true,
-		    animateIn: 'fadeIn',
-            animateOut: 'fadeOut',
-            lazyLoad: true,
-    		responsiveClass:true,
-		    responsive:{
-		        0:{
-		            items: 1
-		        },
-		        600:{
-		            items: 1
-		        },
-		        1000:{
-		            items: 1
-		        }
-		    }
+			loop: true,
+			margin: 10,
+			autoplay: true,
+			animateIn: 'fadeIn',
+			animateOut: 'fadeOut',
+			lazyLoad: true,
+			responsiveClass:true,
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 1
+				},
+				1000:{
+					items: 1
+				}
+			}
 		})
 
 	});
