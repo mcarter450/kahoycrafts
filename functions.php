6e87fda01ad8a689dc2fac151814e81208d5a2cb
Michael Carter <mikeaaroncarter@gmail.com>
2022-02-27 23:12:53 -0800

    Add google aw conversion tracking

--- functions.php
+++ functions.php
@@ -105,3 +105,33 @@ function kahoycrafts_disable_sitemap_specific_page($args, $post_type) {
 	return $args;
 }
 
+/**
+ * Conversion tracking for WC order
+ */
+function add_gtag_purchase_event( $order_id ) {
+
+	if (! $order_id ) {
+		return; // no order id
+	}
+
+	if ( $order = wc_get_order( $order_id ) ) {
+
+		$script = "
+<script>
+gtag('event', 'conversion', {
+	'send_to': 'AW-10818559065/WOdmCKLJo6UDENm42KYo',
+	'value': {$order->get_total()},
+	'currency': '{$order->get_currency()}',
+	'transaction_id': '{$order_id}'
+});
+</script>
+		";
+
+		echo $script;
+
+	}
+
+}
+
+//add_action( 'woocommerce_before_checkout_form', 'add_gtag_purchase_event' );
+add_action( 'woocommerce_thankyou', 'add_gtag_purchase_event' );
