c3318f5bd2e578888da16feaeee7724530a50ac4
Michael Carter <mikeaaroncarter@gmail.com>
2022-03-21 01:38:05 -0700

    Set value of newsletter optin to string

--- functions.php
+++ functions.php
@@ -314,7 +314,8 @@ add_action('woocommerce_after_order_notes', 'newsletter_checkout_field');
 function newsletter_checkout_field_update_order_meta($order_id) {
 
 	if ( isset($_POST['newsletter_optin']) ) {
-		update_post_meta( $order_id, 'newsletter_optin', sanitize_key($_POST['newsletter_optin']) );
+		$value = intval($_POST['newsletter_optin']) ? 'yes' : 'no';
+		update_post_meta( $order_id, 'newsletter_optin', $value );
 	}
 
 }
