27e87a3c325c89ace7e811c4687a56eedfefc9db
Michael Carter <mikeaaroncarter@gmail.com>
2022-05-23 21:30:21 -0700

    Remove jquery migrate library

--- functions.php
+++ functions.php
@@ -82,6 +82,20 @@ function is_blog () {
     return ( is_archive() || is_author() || is_category() || is_home() || is_single() || is_tag()) && 'post' == get_post_type();
 }
 
+function remove_jquery_migrate( $scripts ) {
+	
+	if ( ! is_admin() && isset( $scripts->registered['jquery'] ) ) {
+			
+		$script = $scripts->registered['jquery'];
+		
+		if ( $script->deps ) { 
+			$script->deps = array_diff( $script->deps, array( 'jquery-migrate' ) );
+		}
+	}
+}
+
+add_action( 'wp_default_scripts', 'remove_jquery_migrate' );
+
 /**
  * Enqueue scripts and styles.
  * 
